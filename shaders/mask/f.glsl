2f3ba2e040164f20e0515f831b31bf6d4ff6ec91
andrew-smith <andrew@fisherboy.co.nz>
2011-12-18 00:57:41 +0000

    Cleaned up some old folders.

--- shaders/mask/f.glsl
+++ shaders/mask/f.glsl
@@ -23,6 +23,8 @@ void main(void)
 		gl_FragColor = vec4(1.0, 0.0, 0.0 , 1.0);
 	}
 	
+	gl_FragColor = pos;
+	
 	/*
 	vec2 pos = mod(gl_FragCoord.xy, vec2(50.0)) - vec2(25.0);
       float dist_squared = dot(pos, pos);
--- shaders/mask/v.glsl
+++ shaders/mask/v.glsl
@@ -1,10 +1,21 @@
 
+
+uniform float playerX;
+uniform float playerY;
+
 //pass along to frag shader
 varying vec4 pos;
 
 
+const vec4 C_BLACK = vec4(0.0,0.0,0.0,1.0);
+const vec4 C_TRANS = vec4(0.0,0.0,0.0,0.0);
+
 void main(void)
 {
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
-	pos = gl_Position;
+	vec2 player = vec2(playerX, playerY);
+	pos = mix(C_BLACK, C_TRANS, distance(player,gl_Position)/100);
+	
+	
+	
 }
